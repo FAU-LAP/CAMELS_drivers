c2a2fcee4f1f5878cfa4961924e52d36bd6c978f
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2023-02-15 16:19:43 +0100

    first try for agilent_34401

---
+++ agilent_34401/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Agilent_34401_CAMELS',
+    version='0.1',
+    description='This packes provides everything to run an Agilent 34401 with'
+                'CAMELS',
+    author='Johannes Lehmeyer / Alexander Fuchs',
+    author_email='johannes.lehmeyer@fau.de',
+    packages=find_packages(),
+    install_requires=[]
+)
