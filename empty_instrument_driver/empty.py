062375c4cb793dd63124458f2001a495b4ad8f83
A-D-Fuchs <alexander.d.fuchs@fau.de>
2023-05-12 08:46:57 +0200

    enhanced empty files

--- empty_instrument_driver/empty.py
+++ empty_instrument_driver/empty.py
@@ -3,20 +3,43 @@ from nomad_camels.main_classes import device_class
 
 class subclass(device_class.Device):
     def __init__(self, **kwargs):
-        files = []
-        req = []
         super().__init__(name='instrument_name', virtual=False,
+                         # Change the tags to fit your device
                          tags=['Function', 'Sine', 'Waveform', 'Generator', 'Voltage'],
                          directory='instrument_name',
                          ophyd_device=instrument_name,
                          ophyd_class_name='instrument_name', **kwargs)
+        # Configs are optional
+        # This config setting is changeable in the 'Configure Instrument' window
+        # It is a simple text edit field where you can enter new numbers
+        self.config['custom_signal_config'] = 1
+        # This is a checkbox to set to True or uncheck to set false
+        self.config['custom_signal_config'] = True
+        # This is a combobox
+        # the definition of possible elements is done below in the subclass_config
+        self.config['Source_Type'] = 'Voltage'
+
 
 
 
 class subclass_config(device_class.Simple_Config):
+    """
+    Automatically creates a GUI for the configuration values given here.
+    This is perfect for simple devices with just a few settings.
+    """
     def __init__(self, parent=None, data='', settings_dict=None,
                  config_dict=None, ioc_dict=None, additional_info=None):
+        # Change the 'instrument name' string to the name of the instrument
+        # this will be displayed when adding the instrument in CAMELS
         super().__init__(parent, 'instrument name', data, settings_dict,
                          config_dict, ioc_dict, additional_info)
+        # Optional Configs
+        comboBoxes = {'Source_Type': ["Voltage", "Current", "Sweep Voltage", "Sweep Current"]}
+        labels = {'custom_signal_config': 'Custom Signal Config (V)'}
+        super().__init__(parent, 'Custom Signal Config', data, settings_dict, config_dict, ioc_dict,
+                         additional_info, comboBoxes=comboBoxes, labels=labels)
+
+        # Keep the following line!
+        # or you can not set the VISA communication configuration
         self.comboBox_connection_type.addItem('Local VISA')
         self.load_settings()
--- empty_instrument_driver/empty_ophyd.py
+++ empty_instrument_driver/empty_ophyd.py
@@ -3,6 +3,7 @@ from ophyd import Component as Cpt
 from nomad_camels.bluesky_handling.visa_signal import (VISA_Signal_Write,
                                                        VISA_Signal_Read,
                                                        VISA_Device)
+from nomad_camels.bluesky_handling.custom_function_signal import Custom_Function_Signal
 
 
 
@@ -10,6 +11,14 @@ class instrument_name(VISA_Device):
 
     # This is an example for a standard identification query
     get_ID = Cpt(VISA_Signal_Read, name='get_ID', query_text='*IDN?', metadata={'ID': 'string'})
+
+    # Example of complicated set channel
+    complicated_set = Cpt(VISA_Signal_Write, name='complicated_set',
+                          metadata={'units': 'junk units'})
+
+    # Custom functions do not talk directly to the instrument
+    # and can be used to store config settings
+    custom_signal_config = Cpt(Custom_Function_Signal, name='custom_signal_config', kind='config')
 
     def __init__(self, prefix='', *, name, kind=None, read_attrs=None,
                  configuration_attrs=None, parent=None, resource_name='',
@@ -20,6 +29,17 @@ class instrument_name(VISA_Device):
                          resource_name=resource_name, baud_rate=baud_rate,
                          write_termination=write_termination,
                          read_termination=read_termination, **kwargs)
+        self.complicated_set.put_conv_function = self.complicated_set_function
+
+
+    def complicated_set_function(self, set_value) -> str:
+        # Create a string ehre that is passed to the instrument
+        # Function must return a string
+
+        # Here we get the config saved in 'custom_signal_config'
+        setting_value = self.custom_signal_config.get()
+        created_string = f'set this {set_value} with this setting {setting_value}'
+        return created_string
 
 
 if __name__ == '__main__':
