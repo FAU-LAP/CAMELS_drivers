7542482956b6f3999f35eaed54f35b0bd90ad89f
A-D-Fuchs <alexander.d.fuchs@fau.de>
2023-06-30 14:17:31 +0200

    updated channel names and built version 0.1.1

--- agilent_34401a/nomad_camels_driver_agilent_34401a/agilent_34401a_ophyd.py
+++ agilent_34401a/nomad_camels_driver_agilent_34401a/agilent_34401a_ophyd.py
@@ -6,14 +6,15 @@ from nomad_camels.bluesky_handling.visa_signal import VISA_Signal, VISA_Signal_R
 
 
 class Agilent_34401(VISA_Device):
-    mesDCV = Cpt(VISA_Signal_RO, name='mesDCV', query="MEAS:VOLT:DC?", metadata={'units': 'V'})
-    mesDCI = Cpt(VISA_Signal_RO, name='mesDCI', query="MEAS:CURR:DC?", metadata={'units': 'A'})
-    mesACV = Cpt(VISA_Signal_RO, name='mesACV', query="MEAS:VOLT:AC?", metadata={'units': 'V'})
-    mesACI = Cpt(VISA_Signal_RO, name='mesACI', query="MEAS:CURR:AC?", metadata={'units': 'A'})
-    mesR = Cpt(VISA_Signal_RO, name='mesR', query="MEAS:RES?", metadata={'units': 'Ohm'})
-    mesR4w = Cpt(VISA_Signal_RO, name='mesR4w', query="MEAS:FRES?", metadata={'units': 'Ohm'})
-    idn = Cpt(VISA_Signal_RO, name='idn', kind='config', query='*IDN?')
-    nPLC = Cpt(VISA_Signal, name='nPLC', kind='config')
+    measure_voltage_DC = Cpt(VISA_Signal_RO, name='measure_voltage_DC', query="MEAS:VOLT:DC?", metadata={'units': 'V', 'description': 'Measures DC voltage. Sends the MEAS:VOLT:DC? command.'})
+    measure_current_DC = Cpt(VISA_Signal_RO, name='measure_current_DC', query="MEAS:CURR:DC?", metadata={'units': 'A', 'description': 'Measures DC current. Sends the MEAS:CURR:DC? command.'})
+    measure_voltage_AC = Cpt(VISA_Signal_RO, name='measure_voltage_AC', query="MEAS:VOLT:AC?", metadata={'units': 'V'})
+    measure_current_AC = Cpt(VISA_Signal_RO, name='measure_current_AC', query="MEAS:CURR:AC?", metadata={'units': 'A'})
+    measure_resistance = Cpt(VISA_Signal_RO, name='measure_resistance', query="MEAS:RES?", metadata={'units': 'Ohm'})
+    measure_resistance_4wire = Cpt(VISA_Signal_RO, name='measure_resistance_4wire', query="MEAS:FRES?", metadata={'units': 'Ohm'})
+    device_ID = Cpt(VISA_Signal_RO, name='device_ID', kind='config', query='*IDN?')
+    nPLC = Cpt(VISA_Signal, name='nPLC', kind='config',
+               write='VOLT:DC:NPLC {value};CURR:DC:NPLC {value};RES:NPLC {value}')
 
     def __init__(self, prefix='', *, name, kind=None, read_attrs=None,
                  configuration_attrs=None, parent=None, resource_name='',
