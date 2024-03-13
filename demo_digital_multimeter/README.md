5d3bc6c144c80a5332a60a0e4816ce21ef68cc53
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2024-03-13 11:44:37 +0100

    Add new instrument drivers for camels sandbox

---
+++ demo_digital_multimeter/README.md
@@ -0,0 +1,8 @@
+# NOMAD CAMELS driver for a demo digital multimeter
+
+Simulated demo digital multimeter for testing, written for the measurement software [NOMAD CAMELS](https://fau-lap.github.io/NOMAD-CAMELS/).
+
+
+## Documentation
+
+For more information and documentation visit the NOMAD CAMELS [documentation](https://fau-lap.github.io/NOMAD-CAMELS/doc/instruments/instruments.html).
---
+++ demo_digital_multimeter/nomad_camels_driver_demo_digital_multimeter/demo_digital_multimeter_ophyd.py
@@ -0,0 +1,50 @@
+from ophyd import Component as Cpt
+
+
+import sys
+
+sys.path.append(r"C:\Users\od93yces\FAIRmat")
+sys.path.append(r"C:\Users\od93yces\FAIRmat\CAMELS_sandbox")
+sys.path.append(r"C:\Users\od93yces\FAIRmat\CAMELS_sandbox\CAMELS_sandbox")
+
+from nomad_camels_sandbox.server_signals import (
+    Demo_Server_Signal,
+    Demo_Server_SignalRO,
+    Demo_Server_Device,
+)
+
+
+class Demo_DMM(Demo_Server_Device):
+    # V_DC = Cpt(
+    #     VISA_Signal_RO, name="V_DC", parse=r".*?([-+eE\d.]+).*", metadata={"units": "V"}
+    # )
+    # V_AC = Cpt(
+    #     VISA_Signal_RO, name="V_AC", parse=r".*?([-+eE\d.]+).*", metadata={"units": "V"}
+    # )
+    # I_DC = Cpt(
+    #     VISA_Signal_RO, name="I_DC", parse=r".*?([-+eE\d.]+).*", metadata={"units": "A"}
+    # )
+    # I_AC = Cpt(
+    #     VISA_Signal_RO, name="I_AC", parse=r".*?([-+eE\d.]+).*", metadata={"units": "A"}
+    # )
+    resistance = Cpt(
+        Demo_Server_SignalRO,
+        name="resistance",
+        parameter_name="dmm_pt1000.R",
+        metadata={"units": "Ohm"},
+    )
+    # resistance_4_wire = Cpt(
+    #     VISA_Signal_RO,
+    #     name="resistance_4_wire",
+    #     parse=r".*?([-+eE\d.]+).*",
+    #     metadata={"units": "Ohm"},
+    # )
+
+    # idn = Cpt(VISA_Signal_RO, name="idn", kind="config", query="*IDN?")
+    NPLC = Cpt(
+        Demo_Server_Signal, name="NPLC", kind="config", parameter_name="dmm_pt1000.NPLC"
+    )
+
+
+if __name__ == "__main__":
+    testk = Demo_DMM(name="testk")
