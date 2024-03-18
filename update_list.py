86d70bfd5b39d90e4f5a2462e7059feebcbbb53a
Jo-Leh <89379950+Jo-Leh@users.noreply.github.com>
2024-03-18 14:41:04 +0100

    added update_list.py and twerked some versions for testing automatic update of versions

---
+++ update_list.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def get_latest_version(package_name):
+    response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+    return response.json()["info"]["version"]
+
+
+with open("driver_list.txt", "r") as file:
+    lines = file.readlines()
+
+with open("driver_list.txt", "w") as file:
+    for line in lines:
+        package_name, _ = line.split("==")
+        latest_version = get_latest_version(package_name)
+        file.write(f"{package_name}=={latest_version}\n")
