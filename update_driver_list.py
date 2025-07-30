a26592a0dbcf4d5a554ba0634fa2848e3892817a
Johannes Lehmeyer <89379950+Jo-Leh@users.noreply.github.com>
2025-07-30 10:59:41 +0200

    updated updating of driver list

--- update_driver_list.py
+++ update_driver_list.py
@@ -14,7 +14,6 @@ def get_latest_version(package_name):
     return response.json()["info"]["version"]
 
 
-
 driver_list = []
 
 for f in pathlib.Path(os.path.dirname(__file__)).rglob("pyproject.toml"):
@@ -36,15 +35,30 @@ print(driver_list)
 with open("driver_list.txt", "w") as f:
     f.writelines(driver_list)
 
+url = "https://raw.githubusercontent.com/FAU-LAP/CAMELS_drivers/driver_list/driver_list.txt"
+online_driver_list = requests.get(url).text.splitlines()
+online_drivers = []
+online_versions = []
+for i, d in enumerate(online_driver_list):
+    val = d.strip().split("==")
+    online_drivers.append(val[0])
+    online_versions.append(val[1])
+
+print("\n\n`Name`\t`Version in branch`\t`version on pypi`\t`version on driver list`")
 
 for d in driver_list:
     name, version = d[:-1].split("==")
     last_v = get_latest_version(name)
-    if version == last_v:
+    list_v = (
+        online_versions[online_drivers.index(name)]
+        if name in online_drivers
+        else "None"
+    )
+    if version == last_v and version == list_v:
         sys.stdout.write(
-            "\x1b[1;32m" + f"{name}\t{version}\t{last_v}" + "\x1b[0m" + "\n"
+            "\x1b[1;32m" + f"{name}\t{version}\t{last_v}\t{list_v}" + "\x1b[0m" + "\n"
         )
     else:
         sys.stdout.write(
-            "\x1b[1;31m" + f"{name}\t{version}\t{last_v}" + "\x1b[0m" + "\n"
+            "\x1b[1;31m" + f"{name}\t{version}\t{last_v}\t{list_v}" + "\x1b[0m" + "\n"
         )
